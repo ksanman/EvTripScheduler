9c630aacf75dadaf0baa8bfa1a9a099a29e87d86
Kody Sanchez <agent.xaber@gmail.com>
2019-03-03 00:21:51 -0700

    fixed references

--- trip_scheduler/__init__.py
+++ trip_scheduler/__init__.py
@@ -1,6 +1,6 @@
 from .environment import SimpleEnvironment, EvTripScheduleEnvironment
 from .optimizer import Optimizer
-from .trip_builder import SimpleTripBuilder, Trip, Stop, ChargerConnection, FileTripBuilder, SimpleVehicle, NissanLeaf
+from .trip_builder import SimpleTripBuilder, Trip, Stop, ChargerConnection, FileTripBuilder, SimpleVehicle, NissanLeaf, OsrmTripBuilder
 from .trip_builder.routing import Route
 from .visualizations import Visualize
 from trip_scheduler import TripScheduler
